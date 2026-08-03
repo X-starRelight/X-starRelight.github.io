d5a694bd6894444ad0a929864cfb42f1712b5bbc
sxxyrry <sxxyrry@gmail.com>
2026-08-03 19:29:01 +0800

    更新网站

--- assets/ShowcaseData.js
+++ assets/ShowcaseData.js
@@ -1,12 +1,12 @@
 const ShowcaseData = [
     {
         id: 1,
-        title: "牢尘LC_laochen的官网",
-        description: "我们终此一生，就是要摆脱他人的期待，找到真正的自己。",
-        image: "https://dmxiaozi.top/img/bj1.webp",
-        link: "https://dmxiaozi.top/",
+        title: "广告位招租",
+        description: "广告位招租",
+        image: "https://example.com/image.png",
+        link: "https://example.com",
         buttonText: "了解更多",
-        bg: "url(\"https://dmxiaozi.top/img/bj1.webp\")"
+        bg: "linear-gradient(135deg, #4735e6 0%, #8fee42 80%)"
     },
     {
         id: 2,
